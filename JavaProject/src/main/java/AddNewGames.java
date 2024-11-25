f7b272ca8fd6ec698146740f9506e307487c282c
Daniel <pynadg@students.fscj.edu>
2024-11-25 13:34:13 -0500

    Creating a new class to make program more machine independent. Handle changes in file paths.

--- JavaProject/src/main/java/AddNewGames.java
+++ JavaProject/src/main/java/AddNewGames.java
@@ -3,27 +3,77 @@ import java.util.*;
 import com.fasterxml.jackson.databind.*;
 import com.opencsv.*;
 
-public class AddNewGames {
-    //Input validation method that checks to see if the user inputs an actual file path, ensures there is a real file there.
-    private static String checkPath(String filePath){
+class DefaultPath{
+    public static String defaultTXTpath = "C:\\Users\\Daniel\\OneDrive\\Desktop\\";
+    public static String defaultTXTname = "C:\\Users\\Daniel\\OneDrive\\Desktop\\test.txt";
+    public static String defaultJSON = "C:\\Users\\Daniel\\OneDrive\\Desktop\\test.json";
+    public static String defaultCSV = "C:\\Users\\Daniel\\OneDrive\\Desktop\\Test.csv";
+
+    private static Boolean fileTypes(String filePath){
+        String[] ftArray = {".exe", ".json", ".txt", ".csv"};
+        for (String type: ftArray){if (filePath.endsWith(type)) return true;}
+        return false;
+    }
+
+    public static String checkPath(String filePath){
         Scanner scanner = new Scanner(System.in);
         File check = new File(filePath);
         Boolean type = fileTypes(filePath);
         //If statement that validates the input, returns correctPath if they input something, other than a filepath.
         if (type && check.exists()) {return filePath;}
         else{//recursion
-                System.err.println("Please enter a valid file path. Be sure to that it leads to an exe, json, txt, or csv file.");
-                filePath = scanner.nextLine();
-                checkPath(filePath);
-            }
-            return filePath;
-        
+            System.err.println("Please enter a valid file path. Be sure to that it leads to the stated default file path type.");
+            filePath = scanner.nextLine();
+            checkPath(filePath);
+        }
+        return filePath;
     }
-    private static Boolean fileTypes(String filePath){
-        String[] ftArray = {".exe", ".json", ".txt", ".csv"};
-        for (String type: ftArray){if (filePath.endsWith(type)) return true;}
-        return false;
+
+    public static String getTXTpath() {
+        Scanner scans = new Scanner(System.in);
+        File txt = new File(defaultTXTpath);
+        if (!txt.exists()) {
+            System.out.println("There is no default TXT file for game paths, please enter the file path to one now");
+            defaultTXTpath = scans.nextLine();
+            return checkPath(defaultTXTpath);
+        }
+        return defaultTXTpath;
     }
+
+    public static String getTXTname() {
+        Scanner scans = new Scanner(System.in);
+        File txt = new File(defaultTXTname);
+        if (!txt.exists()) {
+            System.out.println("There is no default TXT file for game name file, please enter the file path to one now");
+            defaultTXTname = scans.nextLine();
+            return checkPath(defaultTXTname);
+        }
+        return defaultTXTname;
+    }
+
+    public static String getJSON() {
+        Scanner scans = new Scanner(System.in);
+        File json = new File(defaultJSON);
+        if (!json.exists()) {
+            System.out.println("There is no default JSON file, please enter the file path to one now /n");
+            defaultJSON = scans.nextLine();
+            return checkPath(defaultJSON);
+        }
+        return defaultJSON;
+    }
+    public static String getCSV() {
+        Scanner scans = new Scanner(System.in);
+        File csv = new File(defaultCSV);
+        if (!csv.exists()) {
+            System.out.println("There is no default CSV file, please enter the file path to one now /n");
+            defaultCSV = scans.nextLine();
+            return checkPath(defaultCSV);
+        }
+        return defaultCSV;
+    }
+}
+
+public class AddNewGames {
 
     //method that counts lines in the text file. When this program opens, this method counts the amount of games already in there and adds a corresponding number.
     private static int countLines(String filePath){
@@ -33,18 +83,13 @@ public class AddNewGames {
             while (br.readLine() != null) gameCount++;
         }catch (IOException e){
             System.out.println("Can not count lines in text file.");
-        } finally{
-
         }
         return gameCount;
-
     }
 
     public static void updateJSON(String gameName, String gamePath){
        try {
-           //make a new file path for my json file, JSON object to store gameName gamePath variables
-           //String jsonFilePath = "C:\\Users\\Daniel\\OneDrive\\Games\\GamePathJSON.json";
-           String jsonFilePath = checkPath("C:\\Users\\Daniel\\OneDrive\\Desktop\\test.json");
+           String jsonFilePath = DefaultPath.getJSON();
            //Mapper from the jackson library will read and write our games
            ObjectMapper mapper = new ObjectMapper();
            Map<String, String> newGames = mapper.readValue(new File(jsonFilePath), Map.class);
@@ -57,10 +102,8 @@ public class AddNewGames {
 
     //this method updates the text files on the computer
     public static void updateTXT(String gameName, String gamePath){
-        //String txtPath = checkPath("C:\\Users\\Daniel\\OneDrive\\Desktop\\Games\\GameFilePath.txt");
-        //String txtName = checkPath("C:\\Users\\Daniel\\OneDrive\\Desktop\\Games\\GameListTXT.txt");
-        String txtPath = checkPath("C:\\Users\\Daniel\\OneDrive\\Desktop\\test2.txt");
-        String txtName = checkPath("C:\\Users\\Daniel\\OneDrive\\Desktop\\test.txt");
+        String txtPath = DefaultPath.getTXTpath();
+        String txtName = DefaultPath.getTXTname();
         try {
             FileWriter gP = new FileWriter(txtPath, true);
             FileWriter gN = new FileWriter(txtName, true);
@@ -75,7 +118,7 @@ public class AddNewGames {
 
     public static void updateCSV(String gameName, String gamePath) {
         //assign the file to a variable & make sure the file exists and is a valid type.
-        String csvPath = checkPath("C:\\Users\\Daniel\\OneDrive\\Desktop\\Test.csv");
+        String csvPath = DefaultPath.getCSV();
         //try resource reads the file in
         try(FileReader fr = new FileReader(csvPath)) {
             //creating csv reader
@@ -109,12 +152,12 @@ public class AddNewGames {
         System.out.println("This java program adds a new game and an associated file path to files associated with the PMFG\\Game Runner programs. \n");
         System.out.println("Please enter the game name:");
         String gameName = scanner.nextLine();
-        System.out.println("Please enter the game file path:");
+        System.out.println("Please enter the game file path: (Note: the game file must be an executable (.exe))");
         String gamePath = scanner.nextLine();
-
-        updateTXT(gameName, checkPath(gamePath));
-        updateJSON(gameName, checkPath(gamePath));
-        updateCSV(gameName, checkPath(gamePath));
+        String validatedPath = DefaultPath.checkPath(gamePath);
+        updateTXT(gameName, validatedPath);
+        updateJSON(gameName, validatedPath);
+        updateCSV(gameName, validatedPath);
         System.out.println("Files updated successfully");
         
     }
