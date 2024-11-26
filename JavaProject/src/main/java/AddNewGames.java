cff0c526958d0086f4f23dd0d74377f5d2bb0d0d
Daniel <pynadg@students.fscj.edu>
2024-11-25 22:27:22 -0500

    Creating a new class to make program more machine independent. Handle changes in file paths.

--- JavaProject/src/main/java/AddNewGames.java
+++ JavaProject/src/main/java/AddNewGames.java
@@ -3,75 +3,6 @@ import java.util.*;
 import com.fasterxml.jackson.databind.*;
 import com.opencsv.*;
 
-class DefaultPath{
-    public static String defaultTXTpath = "C:\\Users\\Daniel\\OneDrive\\Desktop\\";
-    public static String defaultTXTname = "C:\\Users\\Daniel\\OneDrive\\Desktop\\test.txt";
-    public static String defaultJSON = "C:\\Users\\Daniel\\OneDrive\\Desktop\\test.json";
-    public static String defaultCSV = "C:\\Users\\Daniel\\OneDrive\\Desktop\\Test.csv";
-
-    private static Boolean fileTypes(String filePath){
-        String[] ftArray = {".exe", ".json", ".txt", ".csv"};
-        for (String type: ftArray){if (filePath.endsWith(type)) return true;}
-        return false;
-    }
-
-    public static String checkPath(String filePath){
-        Scanner scanner = new Scanner(System.in);
-        File check = new File(filePath);
-        Boolean type = fileTypes(filePath);
-        //If statement that validates the input, returns correctPath if they input something, other than a filepath.
-        if (type && check.exists()) {return filePath;}
-        else{//recursion
-            System.err.println("Please enter a valid file path. Be sure to that it leads to the stated default file path type.");
-            filePath = scanner.nextLine();
-            checkPath(filePath);
-        }
-        return filePath;
-    }
-
-    public static String getTXTpath() {
-        Scanner scans = new Scanner(System.in);
-        File txt = new File(defaultTXTpath);
-        if (!txt.exists()) {
-            System.out.println("There is no default TXT file for game paths, please enter the file path to one now");
-            defaultTXTpath = scans.nextLine();
-            return checkPath(defaultTXTpath);
-        }
-        return defaultTXTpath;
-    }
-
-    public static String getTXTname() {
-        Scanner scans = new Scanner(System.in);
-        File txt = new File(defaultTXTname);
-        if (!txt.exists()) {
-            System.out.println("There is no default TXT file for game name file, please enter the file path to one now");
-            defaultTXTname = scans.nextLine();
-            return checkPath(defaultTXTname);
-        }
-        return defaultTXTname;
-    }
-
-    public static String getJSON() {
-        Scanner scans = new Scanner(System.in);
-        File json = new File(defaultJSON);
-        if (!json.exists()) {
-            System.out.println("There is no default JSON file, please enter the file path to one now /n");
-            defaultJSON = scans.nextLine();
-            return checkPath(defaultJSON);
-        }
-        return defaultJSON;
-    }
-    public static String getCSV() {
-        Scanner scans = new Scanner(System.in);
-        File csv = new File(defaultCSV);
-        if (!csv.exists()) {
-            System.out.println("There is no default CSV file, please enter the file path to one now /n");
-            defaultCSV = scans.nextLine();
-            return checkPath(defaultCSV);
-        }
-        return defaultCSV;
-    }
-}
 
 public class AddNewGames {
 
@@ -89,7 +20,7 @@ public class AddNewGames {
 
     public static void updateJSON(String gameName, String gamePath){
        try {
-           String jsonFilePath = DefaultPath.getJSON();
+           String jsonFilePath = DefaultPath.defaultJSON;
            //Mapper from the jackson library will read and write our games
            ObjectMapper mapper = new ObjectMapper();
            Map<String, String> newGames = mapper.readValue(new File(jsonFilePath), Map.class);
@@ -102,8 +33,8 @@ public class AddNewGames {
 
     //this method updates the text files on the computer
     public static void updateTXT(String gameName, String gamePath){
-        String txtPath = DefaultPath.getTXTpath();
-        String txtName = DefaultPath.getTXTname();
+        String txtPath = DefaultPath.defaultTXTpath;
+        String txtName = DefaultPath.defaultTXTname;
         try {
             FileWriter gP = new FileWriter(txtPath, true);
             FileWriter gN = new FileWriter(txtName, true);
@@ -118,7 +49,7 @@ public class AddNewGames {
 
     public static void updateCSV(String gameName, String gamePath) {
         //assign the file to a variable & make sure the file exists and is a valid type.
-        String csvPath = DefaultPath.getCSV();
+        String csvPath = DefaultPath.defaultCSV;
         //try resource reads the file in
         try(FileReader fr = new FileReader(csvPath)) {
             //creating csv reader
@@ -149,6 +80,7 @@ public class AddNewGames {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        DefaultPath.defaultMain();
         System.out.println("This java program adds a new game and an associated file path to files associated with the PMFG\\Game Runner programs. \n");
         System.out.println("Please enter the game name:");
         String gameName = scanner.nextLine();
